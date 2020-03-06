9d9b17792484870d64570645dce826480f0ec816
WMXPY <wm@mengw.io>
2020-03-06 13:14:30 -0600

    init code

---
+++ src/components/title.tsx
@@ -0,0 +1,19 @@
+/**
+ * @author WMXPY
+ * @namespace Print_Friendly
+ * @description Title
+ */
+
+import * as React from "react";
+
+export type PrintFriendlyTitleProps = {
+
+};
+
+export class PrintFriendlyTitle extends React.PureComponent<PrintFriendlyTitleProps> {
+
+    public render() {
+
+        return <div></div>;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @author WMXPY
+ * @namespace Print_Friendly
+ * @description Index
+ */
+
+export * from "./components/title";
+
