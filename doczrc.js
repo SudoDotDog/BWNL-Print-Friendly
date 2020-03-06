1870cf5015808a16099b4d64ba0dea27d68c2c23
WMXPY <wm@mengw.io>
2020-03-06 13:25:21 -0600

    add title component

--- doczrc.js
+++ doczrc.js
@@ -1,11 +1,11 @@
 /**
  * @author WMXPY
- * @namespace Print
+ * @namespace Print_Friendly
  * @description DOCZ_RC
  */
 
 export default {
-    title: 'BWNL Print',
+    title: 'BWNL Print Friendly',
     filterComponents: (files) =>
         files.filter(filePath => /[w-]*.(js|jsx|ts|tsx)$/.test(filePath)),
     codeSandbox: false,
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -4,16 +4,27 @@
  * @description Title
  */
 
+import { NAVY } from "@bwnl/shiny-inline";
 import * as React from "react";
 
 export type PrintFriendlyTitleProps = {
 
+    readonly fontSize?: string;
 };
 
 export class PrintFriendlyTitle extends React.PureComponent<PrintFriendlyTitleProps> {
 
     public render() {
 
-        return <div></div>;
+        return (<div style={{
+            borderLeft: `8px solid ${NAVY}`,
+            padding: '5px',
+            paddingLeft: '15px',
+            fontSize: this.props.fontSize ?? '45px',
+            lineHeight: this.props.fontSize ?? '45px',
+            fontWeight: 'bold',
+        }}>
+            {this.props.children}
+        </div>);
     }
 }
