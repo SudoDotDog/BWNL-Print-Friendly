867271303698dd2901f88647772471b9fc32c7dc
WMXPY <wm@mengw.io>
2020-03-11 15:07:22 -0500

    add page break cover mdx

--- src/helper/page-cover.tsx
+++ src/helper/page-cover.tsx
@@ -9,6 +9,7 @@ import * as React from "react";
 export type PrintFriendlyPageCoverProps = {
 
     readonly CSSLinks?: string[];
+    readonly scriptLinks?: string[];
 };
 
 export class PrintFriendlyPageCover extends React.PureComponent<PrintFriendlyPageCoverProps> {
@@ -22,17 +23,31 @@ export class PrintFriendlyPageCover extends React.PureComponent<PrintFriendlyPag
             <body>
                 {this.props.children}
             </body>
+            {this._renderScriptLinks()}
         </html>);
     }
 
     private _renderCSSLinks() {
 
         const links: string[] = this.props.CSSLinks || [];
-        return links.map((link: string, index: number) => (<link
-            key={index}
-            href={link}
-            rel="stylesheet"
-            type="text/css"
-        />));
+        return links.map(
+            (link: string, index: number) => (<link
+                key={index}
+                href={link}
+                rel="stylesheet"
+                type="text/css"
+            />),
+        );
+    }
+
+    private _renderScriptLinks() {
+
+        const links: string[] = this.props.scriptLinks || [];
+        return links.map(
+            (link: string, index: number) => (<script
+                key={index}
+                src={link}
+            />),
+        );
     }
 }
